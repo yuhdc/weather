f77a43022b233edd6530c0d777f93b661d333273
yuhdc <74478717+yuhdc@users.noreply.github.com>
2021-06-26 16:11:19 +0700

    Update utils.js
    http -> https

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 // export const keyApi = "dcd93f38e4b7fc04c77b81c1f0f35fdd";
 export const keyApi = "72aab0236329470989181100210406";
 // export const baseUrl = "http://api.openweathermap.org/data/2.5/forecast?";
-export const baseUrl = "http://api.weatherapi.com/v1/forecast.json?";
+export const baseUrl = "https://api.weatherapi.com/v1/forecast.json?";
